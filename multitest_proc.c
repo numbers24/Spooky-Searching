69273ab3d1ac50c58a3d646c3356a8877e846a53
numbers24 <45888417+numbers24@users.noreply.github.com>
2019-11-19 20:29:04 -0500

    Add files via upload

---
+++ multitest_proc.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int search(int key,int max,int arr[])
+{	
+	int i,j,k,val,temp;
+	if(max%250)
+	j=max/250+1;
+	else
+	j=max/250;
+	
+	for(i=0;i<j;i++)
+	{	
+		if(!fork())
+		{
+			for(k=i*250;k<((i+1)*250)&&k<max;k++)
+			{		
+				if(arr[k]==key)
+				{
+					if(k%250==0)
+					exit(max);						
+					exit(k%250);
+				}
+			}
+			exit(0);
+		}
+		else
+		{
+			wait(&val);
+			if(val)
+			{
+				if(val==max%256*256)
+				val=i*250;
+				else
+				val=WEXITSTATUS(val)+(i*250);
+				printf("Found key %d at index %d\n",arr[val],val);
+				temp=val;			
+			}				
+		}	
+	}	
+	printf("\n");
+	return temp;
+}	
